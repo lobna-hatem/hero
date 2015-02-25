af0e7ebeee1694ae78a8e263c0f90c928f46e8f0
lobna_hatem <lobna.hatem@gmail.com>
2015-02-25 22:47:32 +0000

    deploy

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,28 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+  
+def team
+  # render team
+end
+
+def lobna
+  
+end
+
+def ingy
+end
+
+def farah
+end
+
+def nirvana
+end
+
+def nourhan 
+end
+
+def nihal
+end
+ 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '1be2e89f731d7b7dbf1db438461fb83ac75f6ae494cc4315179d5c6de4284cd602fb8964e95bae9b2d7f35dfbbcf84cf48950748ddf88a69d5c963788bbadee7'
